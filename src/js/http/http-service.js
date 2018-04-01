8da53d82628aef43387bdcf80eebc9da18860afb
avvet <sveta.drozdovich@yandex.ru>
2018-04-01 11:51:35 +0300

    Http Service class sreated

---
+++ src/js/http/http-service.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const BASE_URL = 'http://localhost:3000/';
+const POSTS = 'posts';
+const DIVIDER = '/';
+
+class HttpServiceClass {
+
+  getAllPosts() {
+    axios.get(BASE_URL + POSTS)
+      .then(responce => {
+        console.log(responce.data);
+        getTitles(responce.data);
+      })
+      .catch(error => {
+        console.log(error, 'MY ERROR');
+      });
+  };
+
+
+  getSinglePost(postId) {
+    axios.get(BASE_URL + POSTS + DIVIDER + postId)
+      .then(responce => {
+        console.log(responce.data, 'SINGLE POST');
+      })
+      .catch(error => {
+        console.log(error, 'MY ERROR');
+      });
+  }
+}
+
+
+function getTitles(posts) {
+  posts.map(post => console.log(post.title, 'MY TITLE'));
+}
+
+let httpService = new HttpServiceClass();
+
+export {httpService};
--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,17 @@
 import '../css/main.scss';
 import '../user.html';
+import {httpService} from "./http/http-service";
+
 import $ from 'jquery';
-import axios from "axios";
-
 import {func} from './second';
-
 // func();
-console.log('Hello,world');
 
 let getPostsBtn = document.getElementById('my-button');
 let singlePostBtn = document.getElementById('single-post');
 
 getPostsBtn.onclick = () => {
-  getAllPosts();
+  httpService.getAllPosts();
 };
 singlePostBtn.onclick = () => {
-  getSinglePost(4);
+  httpService.getSinglePost(4);
 };
-
-
-function getAllPosts() {
-  axios.get('http://localhost:3000/posts')
-    .then(responce => {
-      console.log(responce.data);
-      getTitles(responce.data);
-    })
-    .catch(error => {
-      console.log(error, 'MY ERROR');
-    });
-}
-
-function getTitles(posts) {
-  posts.map(post => console.log(post.title, 'MY TITLE'));
-}
-
-
-function getSinglePost(postId) {
-  axios.get('http://localhost:3000/posts' + '/' + postId)
-    .then(responce => {
-      console.log(responce.data, 'SINGLE POST');
-    })
-    .catch(error => {
-      console.log(error, 'MY ERROR');
-    });
-}
